d356221db194711cca40f98033802ea9fc5b978d
lllkaf <lllkaf.vf@gmail.com>
2023-12-11 21:21:32 +0400

    third step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+
+import { askNameUser } from "../src/cli.js";
 console.log('Welcome to the Brain Games!')
+
+askNameUser();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+function askNameUser () {
+   const name = readlineSync.question('May I have your name? ');
+   console.log(`Hello, ${name}!`);
+}
+
+export {askNameUser}
